08cb13b4f6adc2eae17a6b7cb1336d59cf5aed4f
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-08-06 16:29:31 +0530

    delete button fully functional

--- scripts/checkOut.js
+++ scripts/checkOut.js
@@ -13,8 +13,7 @@ cart.forEach((cartItem)=>{
             matchingProduct=product;
         }
     })
-    console.log(matchingProduct);
-    cartHtml += `<div class="cart-item-container">
+    cartHtml += `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -97,5 +96,7 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
   link.addEventListener('click',()=>{
     let productId = link.dataset.productId;
     removeFromCart(productId);
+    const container=document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   })
 })
